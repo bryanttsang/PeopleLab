7e7fde552450e977c0c402cd3c0dcc8c64800712
BT_1N3_21 <jlin3392@bths.edu>
2018-10-23 14:43:08 -0400

    Runner

---
+++ src/Runner.java
@@ -0,0 +1,7 @@
+public class Runner {
+
+    public static void main(String[] args) {
+        Person[] firstNames = {"Name1", "Name2"};
+        Person[] familyNames = {"Surname1", "Surname2"};
+    }
+}
